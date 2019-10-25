4662ba46ee0e84ebf41847c838197a2d3d1f5ea5
GproSoftware828 <JetSendProductions@Gmail.com>
2019-10-25 14:36:06 +1100

    Completed before testing

--- MedicalReadings/Program.cs
+++ MedicalReadings/Program.cs
@@ -13,12 +13,11 @@ namespace MedicalReadings
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Please enter BP to input blood pressure data or KD to enter Kidney Disease data:");
-            string readChoice;
-            readChoice = Console.ReadLine();
             do
             {
+                Console.WriteLine("Please enter BP to input blood pressure data or KD to enter Kidney Disease data:");
+                string readChoice;
+                readChoice = Console.ReadLine();
                 try
                 {
                     if (readChoice == "BP")
@@ -33,7 +32,6 @@ namespace MedicalReadings
                         }
                         JArray inputArray = JArray.Parse(BPObj);
                         //write to a .txt file here
-                        Console.WriteLine(inputArray);
                         List<BloodPressure> bloodPressureCollectionInput = JsonConvert.DeserializeObject<List<BloodPressure>>(BPObj);
                         var Pressure1 = bloodPressureCollectionInput[bloodPressureCollectionInput.Count - 1].SysBP;
                         var Pressure2 = bloodPressureCollectionInput[bloodPressureCollectionInput.Count - 1].DiaBP;
@@ -54,10 +52,17 @@ namespace MedicalReadings
                             && !((159 < Pressure1 && Pressure1 < 180) || (99 < Pressure2 && Pressure2 < 110))
                             && !((139 < Pressure1 && Pressure1 < 160) || (89 < Pressure2 && Pressure2 < 100)))
                         {
-                            Console.WriteLine("No classification of blood pressure: " + bloodPressureCollectionInput);
+                            Console.WriteLine("No hypertension: ");
                         }
+                        var PressureDate = bloodPressureCollectionInput[bloodPressureCollectionInput.Count - 1].atDate;
+                        Console.WriteLine("Referencing reading: SysBP of: " 
+                            + $"{Pressure1}" 
+                            + ", DiaBP of: " 
+                            + $"{Pressure2}" 
+                            + ", at input date: " 
+                            + $"{PressureDate}");
                     }
-                    if (readChoice == "KD")
+                    if(readChoice == "KD")
                     {
                         Console.WriteLine("Please enter eGFR Data in the format [{eFGR: 65, atDate: '2018/10/31'},{eFGR: 70, atDate: '2018/10/20'}]");
                         Console.WriteLine("Enter 'exit' to exit the system.");
@@ -67,44 +72,72 @@ namespace MedicalReadings
                         {
                             break;
                         }
-                        JArray inputArray = JArray.Parse(KDObj);
-                        Console.WriteLine(inputArray);
-                        List<KidneyReadings> kidneyCollectionInput = JsonConvert.DeserializeObject<List<KidneyReadings>>(KDObj);
-                        var Reading1 = kidneyCollectionInput[kidneyCollectionInput.Count - 1].eFGR;
-                        Console.WriteLine(Reading1);
-                        if (Reading1 > 89)
+                        if (KDObj != "exit")
                         {
-                            Console.WriteLine("Normal");
+                            JArray inputArray = JArray.Parse(KDObj);
+                            List<KidneyReadings> kidneyCollectionInput = JsonConvert.DeserializeObject<List<KidneyReadings>>(KDObj);
+                            var Reading1 = kidneyCollectionInput[kidneyCollectionInput.Count - 1].eFGR;
+                            if (Reading1 > 89)
+                            {
+                                Console.WriteLine("Latest reading is: Normal");
+                            }
+                            if (59 < Reading1 && Reading1 < 90)
+                            {
+                                Console.WriteLine("Latest reading is: Mildly Decreased");
+                            }
+                            if (44 < Reading1 && Reading1 < 60)
+                            {
+                                Console.WriteLine("Latest reading is: Mild to Moderate");
+                            }
+                            if (29 < Reading1 && Reading1 < 45)
+                            {
+                                Console.WriteLine("Latest reading is: Moderate to Severe");
+                            }
+                            if (14 < Reading1 && Reading1 < 30)
+                            {
+                                Console.WriteLine("Latest reading is: Severely Decreased");
+                            }
+                            if (!(Reading1 > 89)
+                                && !(59 < Reading1 && Reading1 < 90)
+                                && !(44 < Reading1 && Reading1 < 60)
+                                && !(29 < Reading1 && Reading1 < 45)
+                                && !(14 < Reading1 && Reading1 < 30))
+                            {
+                                Console.WriteLine("Kidney Failure");
+                            }
+                            var Reading1Date = kidneyCollectionInput[kidneyCollectionInput.Count - 1].atDate;
+                            Console.WriteLine("Referencing: e.FGR of: " + $"{Reading1}" + " on date: " + $"{Reading1Date}");
+                            for (int i = 0; i < kidneyCollectionInput.Count; i++)
+                            {
+                                var readingFirst = kidneyCollectionInput[i];
+                                int j = i + 1;
+                                var readingSecond = kidneyCollectionInput[j];
+                                if (readingSecond.eFGR < (readingFirst.eFGR * .81))
+                                {
+                                    Console.WriteLine("DROP OVER 20% FOUND: e.FGR reading " + $"{readingFirst.eFGR}" 
+                                        + " on date" + 
+                                        $"{readingFirst.atDate}" + 
+                                        " and e.FGR reading " + 
+                                        $"{readingSecond.eFGR}" + " on date " +
+                                        $"{readingSecond.atDate}"
+                                        + " show a drop of "
+                                    + Math.Round(((1 - (double)readingSecond.eFGR / (double)readingFirst.eFGR)) * 100) + "%");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Showing no signs of large drops in these consecutive readings.");
+                                }
+                            }
                         }
-                        if (59 < Reading1 && Reading1 < 90)
-                        {
-                            Console.WriteLine("Mildly Decreased");
-                        }
-                        if (44 < Reading1 && Reading1 < 60)
-                        {
-                            Console.WriteLine("Mild to Moderate");
-                        }
-                        if (29 < Reading1 && Reading1 < 45)
-                        {
-                            Console.WriteLine("Moderate to Severe");
-                        }
-                        if (14 < Reading1 && Reading1 < 30)
-                        {
-                            Console.WriteLine("Severely Decreased");
-                        }
-                        if(!(Reading1 > 89) 
-                            && !(59 < Reading1 && Reading1 < 90)
-                            && !(44 < Reading1 && Reading1 < 60)
-                            && !(29 < Reading1 && Reading1 < 45)
-                            && !(14 < Reading1 && Reading1 < 30))
-                        {
-                            Console.WriteLine("Kidney Failure");
-                        }
+                    }
+                    else
+                    {
+                        throw new ArgumentException();
                     }
                 }
                 catch (ArgumentException)
                 {
-                    Console.WriteLine("Please only enter one of the two options.");
+                    Console.WriteLine("Results completed.");
                 }
             } while (1 < 2);
         }
